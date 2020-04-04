14530f9016824294495084631b4aa27196c33844
zMrDevJ <mraciti96@gmail.com>
2020-04-04 22:47:33 +0200

    Improve scheduler and minor fixes

--- Bot.py
+++ Bot.py
@@ -2,7 +2,6 @@
 
 from quoteAPI import getQuoteOfDay
 import tweepy
-import random
 import time
 
 
@@ -18,7 +17,7 @@ class Bot:
 
     # TODO Refactoring
     def postRetweetFromUser(self, user_screen_name, tweetsNo):
-        query = f'from:{user_screen_name} -filter:retweets'
+        query = f'from:{user_screen_name} -is:retweet -is:reply'
         tweets = tweepy.Cursor(self.api.search, query,
                                tweet_mode='extended').items(tweetsNo)
         for tweet in tweets:
@@ -26,31 +25,33 @@ class Bot:
                 tweet.favorite()
                 tweet.retweet()
                 print(f'Retweeted a tweet from {user_screen_name}!')
-                time.sleep(random.randrange(30, 150))
+                time.sleep(20)
             except tweepy.TweepError as e:
                 print(e.reason)
-                time.sleep(60)
+                time.sleep(10)
 
     def postQuoteOfDay(self):
         quote, author = getQuoteOfDay()
         tweet = f'"{quote.lstrip()}"\n~{author}\n\n#QoD #quoteoftheday #quote #seerberos #feeds'
-        self.postStatus(tweet)
-        print('Posted the QoD!')
+        try:
+            self.postStatus(tweet)
+            print('Posted the QoD!')
+        except tweepy.TweepError as e:
+            print(e.reason)
 
-    # TODO Schedule this function with a Cursor update every tot
-    def searchHashtags(self, query, tweetsNo):
+    def searchHashtags(self, query, tweetsNo, followersCount=500):
         tweets = tweepy.Cursor(self.api.search, query,
                                tweet_mode='extended').items(tweetsNo)
         for tweet in tweets:
             try:
                 # A simple account check before retweetting
-                if (tweet.user.followers_count >= 500):
+                if (tweet.user.followers_count >= followersCount):
                     tweet.retweet()
                     print('Retweeted a tweet!')
-                    time.sleep(random.randrange(30, 150))
+                    time.sleep(20)
             except tweepy.TweepError as e:
                 print(e.reason)
-                time.sleep(40)
+                time.sleep(10)
 
     def print(self):
         msg = f'Hello, I\'m {self.user.screen_name}!'
--- main.py
+++ main.py
@@ -2,22 +2,38 @@
 
 from config import getApi
 from Bot import Bot
+import schedule
 import random
 import time
 
-query = '#cybersecurity #infosec -filter:retweets'
+query = '#cybersecurity #infosec OR #cybersecurity OR #infosec OR #pentesting OR #security OR #privacy -is:retweet -is:reply followers_count:500'
 tweetsNo = 10
+trustThershold = 500  # Min number of followers to make the bot retweet
+user_screen_name = 'zMrDevJ'
+
+
+def jobQoD(bot):
+    bot.postQuoteOfDay()
+
+
+def jobRT(bot):
+    bot.searchHashtags(query, tweetsNo, trustThershold)
+
+
+def jobRTFrom(bot):
+    bot.postRetweetFromUser(user_screen_name, tweetsNo)
 
 
 def main():
     bot = Bot(getApi())
 
-    bot.postQuoteOfDay()
-    time.sleep(random.randrange(100, 300))
+    schedule.every().day.at("10:00").do(jobQoD, bot)
+    schedule.every(2).hours.do(jobRTFrom, bot)
+    schedule.every(5).to(10).minutes.do(jobRT, bot)
 
-    while (True):
-        bot.searchHashtags(query, tweetsNo)
-        time.sleep(random.randrange(1800, 3600))
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
